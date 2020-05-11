ceefcfddcab6ed6ea2291a200543913ae549c2ca
karlssonr <karlssonr1989@gmail.com>
2020-05-11 15:36:59 +0200

    added setonclicklistner on imageview

--- app/src/main/java/com/example/receptappen/ActivityCreateRecipe.kt
+++ app/src/main/java/com/example/receptappen/ActivityCreateRecipe.kt
@@ -1,23 +1,39 @@
 package com.example.receptappen
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_create_recipe.*
 import kotlinx.android.synthetic.main.activity_home_screen_recycle.*
-
+import kotlinx.android.synthetic.main.add_photo_row.*
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 
 class ActivityCreateRecipe : AppCompatActivity() {
 
     var db = FirebaseFirestore.getInstance()
-    lateinit var alertDialog: AlertDialog
-    lateinit var storageReference: StorageReference
+
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
+
+
 
 
 
@@ -25,17 +41,54 @@ class ActivityCreateRecipe : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_recipe)
 
-        val addPhotoImage = findViewById<View>(R.id.add_photo_image)
+
+
+        firebaseStore = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+
+
 
         recycler_view_add_photo.layoutManager = LinearLayoutManager(this)
-        recycler_view_add_photo.adapter = AdapterAddPhoto()
+        recycler_view_add_photo.adapter = AdapterAddPhoto(this)
 
-        alertDialog =
 
-        addPhotoImage.setOnClickListener()
 
 
 
 
     }
+
+    private fun launchGallery() {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+        }
+
+        private fun uploadImage(){
+            TODO("Not Implemented")
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if(data == null || data.data == null){
+                return
+            }
+
+            filePath = data.data
+
+            try {
+                add_photo_image.isDrawingCacheEnabled = true
+                add_photo_image.buildDrawingCache()
+                val bitmap = (add_photo_image.drawable as BitmapDrawable).bitmap
+                val baos = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                val data = baos.toByteArray()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/receptappen/AdapterAddPhoto.kt
+++ app/src/main/java/com/example/receptappen/AdapterAddPhoto.kt
@@ -1,26 +1,38 @@
 package com.example.receptappen
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.food_recipe_row.view.*
 
-class AdapterAddPhoto: RecyclerView.Adapter<CustomViewHolder>() {
+class AdapterAddPhoto(private val context: Context): RecyclerView.Adapter<AdapterAddPhoto.ViewHolder>() {
+
+     private val layoutInflater = LayoutInflater.from(context)
 
     override fun getItemCount(): Int {
         return 1
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val layoutInflater = LayoutInflater.from(parent?.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
         val cellForRow = layoutInflater.inflate(R.layout.add_photo_row, parent, false)
-        return CustomViewHolder(cellForRow)
+        return ViewHolder(cellForRow)
 
     }
 
-    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        holder.addPhotoImage.setOnClickListener {
+            println("!!!")
+        }
+
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val addPhotoImage = itemView.findViewById<View>(R.id.add_photo_image)
 
 
     }
