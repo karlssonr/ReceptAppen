e5c4d47d5b3a2759c4b537bd3acafdaba1851ac2
Robin <karlssonr1989@gmail.com>
2020-06-08 21:09:39 +0200

    changed allrecipe choosen to fragment

--- app/src/main/java/com/example/receptappen/ChoosenRecipeFragment.kt
+++ app/src/main/java/com/example/receptappen/ChoosenRecipeFragment.kt
@@ -16,32 +16,7 @@ class ChoosenRecipeFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
 
-        val choosenRecipeImage = view?.findViewById<ImageView>(R.id.imageView_choosen_recipe)
-        val choosenRecipeTitle = view?.findViewById<TextView>(R.id.textView_choosen_recipe_title)
-        val choosenRecipeTime = view?.findViewById<TextView>(R.id.textView_choosen_recipe_time)
-        val choosenRecipeCategory = view?.findViewById<TextView>(R.id.textView_choosen_recipe_category)
-        val choosenRecipeIngrediens = view?.findViewById<TextView>(R.id.textView_choosen_recipe_ingrediens)
-        val choosenRecipeDescription = view?.findViewById<TextView>(R.id.textView_choosen_recipe_description)
 
-
-  /*      val allRecipeImage = intent.getStringExtra("allRecipeImage")
-        val allRecipeTitle = intent.getStringExtra("allRecipeTitle")
-        val allRecipeCategory = intent.getStringExtra("allRecipeCategory")
-        val allRecipeDescription = intent.getStringExtra("allRecipeDescription")
-        val allRecipeIngrediens = intent.getStringExtra("allRecipeIngrediens")
-        val allRecipeTime = intent.getStringExtra("allRecipeCooktime")*/
-
-        val allRecipeImage = arguments?.getString("allRecipeImage")
-        val allRecipeTitle = arguments?.getString("allRecipeTitle")
-        val allRecipeCategory = arguments?.getString("allRecipeCategory")
-       // val allRecipeDescription = arguments?.getString("allRecipeDescription")
-      //  val allRecipeIngrediens = arguments?.getString("allRecipeIngrediens")
-        val allRecipeTime = arguments?.getString("allRecipeTime")
-
-        Picasso.with(activity).load(allRecipeImage).into(choosenRecipeImage)
-        choosenRecipeCategory?.text = allRecipeCategory
-        choosenRecipeTime?.text = allRecipeTime
-        choosenRecipeTitle?.text = allRecipeTitle
 
     }
 
@@ -51,6 +26,38 @@ class ChoosenRecipeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_chosen_recipe, container, false)
         val activity = activity as Context
+
+
+
+        val choosenRecipeImage = view?.findViewById<ImageView>(R.id.imageView_choosen_recipe)
+        val choosenRecipeTitle = view?.findViewById<TextView>(R.id.textView_choosen_recipe_title)
+        val choosenRecipeTime = view?.findViewById<TextView>(R.id.textView_choosen_recipe_time)
+        val choosenRecipeCategory = view?.findViewById<TextView>(R.id.textView_choosen_recipe_category)
+        val choosenRecipeIngrediens = view?.findViewById<TextView>(R.id.textView_choosen_recipe_ingrediens)
+        val choosenRecipeDescription = view?.findViewById<TextView>(R.id.textView_choosen_recipe_description)
+
+
+        /*      val allRecipeImage = intent.getStringExtra("allRecipeImage")
+              val allRecipeTitle = intent.getStringExtra("allRecipeTitle")
+              val allRecipeCategory = intent.getStringExtra("allRecipeCategory")
+              val allRecipeDescription = intent.getStringExtra("allRecipeDescription")
+              val allRecipeIngrediens = intent.getStringExtra("allRecipeIngrediens")
+              val allRecipeTime = intent.getStringExtra("allRecipeCooktime")*/
+
+        val allRecipeImage = arguments?.getString("allRecipeImage")
+        val allRecipeTitle = arguments?.getString("allRecipeTitle")
+        val allRecipeCategory = arguments?.getString("allRecipeCategory")
+        val allRecipeDescription = arguments?.getString("allRecipeDescription")
+        val allRecipeIngrediens = arguments?.getString("allRecipeIngrediens")
+        val allRecipeTime = arguments?.getString("allRecipeTime")
+
+        Picasso.with(activity).load(allRecipeImage).into(choosenRecipeImage)
+        choosenRecipeCategory?.text = allRecipeCategory
+        choosenRecipeTime?.text = allRecipeTime
+        choosenRecipeTitle?.text = allRecipeTitle
+        choosenRecipeDescription?.text = allRecipeDescription
+        choosenRecipeIngrediens?.text = allRecipeIngrediens
+
         return view
     }
 
